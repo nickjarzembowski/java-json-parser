f6a2c88a067b398c23370ea36bf8e6cdc2181d0c
Nicholas Jarzembowski <nicholas.jarzembowski@aexp.com>
2018-05-08 17:11:01 +0100

    more changes to json datastructure

--- src/main/java/json/domain/Json.java
+++ src/main/java/json/domain/Json.java
@@ -6,19 +6,31 @@ import java.util.List;
  * Contains the root node of a json tree
  */
 public class JsonTree {
-    final JsonNode root;
+    
+    private final JsonNode root;
+    
     public JsonTree(final JsonNode root) {
         this.root = root;
     }
     public JsonNode getRoot() { return root.getChildren().get(0); }
+    
     public JsonNode getNode(String key) { return getRoot().getNode(key); }
+    
     public JsonNode.NodeList getNodeList(String key) { return getRoot().getNode(key).getNodeList(); }
+    
     public List<String> getStringList(String key) { return getRoot().getNode(key).getStringList(); }
+    
     public List<Integer> getIntegerList(String key) { return getRoot().getNode(key).getIntegerList(); }
+    
     public boolean containsKey(String key) { return getRoot().containsKey(key); }
+    
     public boolean containsKeys(String... keys) { return getRoot().containsKeys(keys); }
+    
     public int getInt(String key){ return getRoot().getInt(key); }
+    
     public String getString(String key){ return getRoot().getString(key); }
+    
     public Boolean getBoolean(String key){ return getRoot().getBoolean(key); }
+    
     public double getDouble(String key){ return getRoot().getDouble(key); }
 }
